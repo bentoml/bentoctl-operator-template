3bc6594dcc3d97f61efd523191e1936f8ac291ce
Bozhao <yubz86@gmail.com>
2021-11-11 17:56:49 +0000

    Update template to its own module directory and introduce operator_config.py

---
+++ my_operator/__init__.py
@@ -0,0 +1,12 @@
+from .deploy import deploy
+from .update import update
+from .delete import delete
+from .describe import describe
+
+
+__all__ = [
+    "deploy",
+    "update",
+    "describe",
+    "delete"
+]
