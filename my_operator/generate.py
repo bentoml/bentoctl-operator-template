c5b22b8456ec902a619ea862f5f3acc288053704
Bozhao <yubz86@gmail.com>
2022-03-21 16:53:56 +0000

    update

--- my_operator/generate.py
+++ my_operator/generate.py
@@ -1,3 +1,9 @@
 def generate(name, spec, template_type):
+    """
+    Name is the deployment name
+    spec is the deployment spec
+    template_type is the output template format
+    return is path to the generated template
+    """
     generated_path = ""
     return generated_path
