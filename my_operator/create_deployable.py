4be8ba894adc0ce14d5f0a99c55b00fec41f3036
Bozhao <yubz86@gmail.com>
2022-03-31 09:48:55 -0700

    update the create deployable and registry func signatures

--- my_operator/create_deployable.py
+++ my_operator/create_deployable.py
@@ -1,6 +1,6 @@
 import os
 
-def create_deployable():
+def create_deployable(bento_path: str, destination_dir, bento_metadata:dict, overwrite_deployable=True) -> str:
     """
     Create deployable and return path to the deployable, dockerfile, docker context path and additional build args
     """
--- my_operator/registry.py
+++ my_operator/registry.py
@@ -1,4 +1,4 @@
-def get_registry_info(deployment_name: str, bento_name: str, spec):
+def get_registry_info(deployment_name: str, spec: dict):
     registry_url = ''
     username = ''
     password = ''
